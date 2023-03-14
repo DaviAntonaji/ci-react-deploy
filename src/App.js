3f5f93e3a89856c1dfc6b63fe73fb400f4a1d420
Davi Antonaji <wepass@MacBook-Pro-de-Wepass.local>
2023-03-14 14:29:48 -0300

    1.0.1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <p>Versão 1.0.0</p>
+       <p>Versão 1.0.1</p>
       </header>
     </div>
   );
