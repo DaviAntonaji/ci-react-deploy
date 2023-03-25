8701ed2d7040521a6497127a7b44e8b416d4d97f
Davi Antonaji <davi.antonajis@gmail.com>
2023-03-25 18:57:22 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <p>Versão 1.0.4</p>
+       <p>Versão 1.0.5</p>
       </header>
     </div>
   );
